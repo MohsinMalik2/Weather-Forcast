b0b98fe4f57422d1d3395036ed2f6972bdf6db9c
MohsinMalik2 <malikmuhsin70@gmail.com>
2022-02-01 16:22:26 +0500

    commeted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import '../src/asset/style/index.css'
+import '../src/asset/style/style.css'
+const api = {
+  key: "afdcf7e46cf2fd47e04d9173f488a5c1",
+  baseUrl: "https://api.openweathermap.org/data/2.5/"
+}
+
+
 
 function App() {
+
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  const dateSet= (d) => {
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return  ` ${day} , ${date} ${month} ${year}` 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className=''>
+          <h1> Weather Forecast </h1>
+
+          <div className="body">
+              <div className="searchBody">
+                  <input type="search" placeholder='Search Location .....' />
+              </div>
+
+              <div className="Display">
+                  <h4 className='searchedLocation'>
+                    Lahore, Pakistan
+                  </h4>
+                  <h3 className='datePresent'>{dateSet(new Date())} </h3>
+               
+                    <span className='degrees'>
+                        15°
+                    </span>
+           
+                  
+                  <p className='dayStatus'>Sunny or Dusky</p>
+
+              </div>
+          </div>
+      </main>
+        
     </div>
   );
 }
